689c30d5723ac276834b80f3ed5097b090023821
dudadrehmer <cehdrehmer@gmail.com>
2017-11-10 10:34:51 -0200

    Add new box ok

--- main.js
+++ main.js
@@ -4,21 +4,50 @@ var palette = document.getElementById('palette');
 // Add New Color event
 colorBox.addEventListener('submit', addBox);
 
-// Add box event
+//Remove Box Color event
+palette.addEventListener('click', removeBox);
+
+//Verify Hexadecimal ...
+//Apply color to existing Box ...
+//Ajust boxes responsively ...
+
+// Add New Box Color
 function addBox(e){
   e.preventDefault();
-
-  console.log('Hello');
 
   // Get color input
   var newColor = document.getElementById('newColor').value;
 
-  console.log(newColor);
-
-  //create new box
+    //create new box
   var box = document.createElement('div');
   // add classes
   box.className = 'color-box';
-
   console.log(box);
-}
+
+  //create color input
+  var colorInput = document.createElement('input');
+  // add classes
+  colorInput.className = 'box-color box-input';
+
+  // append to box
+  box.appendChild(colorInput);
+
+  // create close button
+  var closeBtn = document.createElement('button');
+  // add Class
+  closeBtn.className = 'close-btn';
+  // add text
+  closeBtn.appendChild(document.createTextNode('X'));
+
+  // append to box
+  box.appendChild(closeBtn);
+
+  // append box to wraper
+  palette.appendChild(box);
+};
+
+// Remove Color Box
+
+function removeBox(){
+  
+};
