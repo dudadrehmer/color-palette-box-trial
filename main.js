151757713e86d7e8456a6fb13dbe243e90cb3a82
dudadrehmer <cehdrehmer@gmail.com>
2017-11-14 11:57:47 -0200

    Added names to boxes adn some input modifications

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var colorBox = document.getElementById('addColor');
 var palette = document.getElementById('palette');
+var newColor = document.getElementsByClassName('new-color');
 
 // Add New Color event
 colorBox.addEventListener('submit', addBox);
@@ -7,38 +8,51 @@ colorBox.addEventListener('submit', addBox);
 //Remove Box Color event
 palette.addEventListener('click', removeBox);
 
-//Verify Hexadecimal ...
-//Apply color to existing Box ...
-//Ajust boxes responsively ...
+document.body.addEventListener('submit' , changeColor);
+
+function changeColor(e){
+  if(e.target.classList.contains('new-color')){
+    for(var i = 0; i < newColor.length; i++){
+      console.log(newColor[i].value);
+    }
+  }
+};
 
 // Add New Box Color
 function addBox(e){
   e.preventDefault();
 
   // Get color input
-  var newColor = document.getElementById('newColor').value;
 
     //create new box
   var box = document.createElement('div');
-  // add classes
   box.className = 'color-box';
-  
-  //create color input
-  var colorInput = document.createElement('input');
-  // add classes
-  colorInput.className = 'box-color box-input';
 
-  // append to box
-  box.appendChild(colorInput);
+  //add name based on background-color
+  var boxName = document.createElement('h3');
+  boxName.innerHTML = document.getElementById('color-input').value;
+  box.appendChild(boxName);
+
+  //color form
+  var changeColorForm = document.createElement('form');
+  box.appendChild(changeColorForm);
+
+  //create color input
+  // var changeColorInput = document.createElement('input');
+  // changeColorInput.className = 'box-color box-input new-color';
+  // changeColorForm.appendChild(changeColorInput);
+
+  //color submit
+  var changeColorSubmit = document.createElement('input');
+  changeColorSubmit.setAttribute('type' , 'color');
+  changeColorSubmit.setAttribute('value' , document.getElementById('color-input').value)
+  // changeColorSubmit.innerHTML = "Change Color"
+  changeColorForm.appendChild(changeColorSubmit);
 
   // create close button
   var closeBtn = document.createElement('button');
-  // add Class
   closeBtn.className = 'close-btn';
-  // add text
   closeBtn.appendChild(document.createTextNode('X'));
-
-  // append to box
   box.appendChild(closeBtn);
 
   // append box to wraper
@@ -46,7 +60,6 @@ function addBox(e){
 };
 
 // Remove Color Box
-
 function removeBox(e){
   if(e.target.classList.contains('close-btn')){
     if(confirm('Remove this color from palette?')){
